gemini-hlsw/explore

Edit single observation constraint set on constraints tab.

common/src/main/scala/explore/model/ModelUndoStacks.scala
@@ -17,27 +17,24 @@ import monocle.Focus
 import scala.collection.immutable.SortedSet
 
 case class ModelUndoStacks[F[_]](
-  forObsList:           UndoStacks[F, ObservationList] = UndoStacks.empty[F, ObservationList],
-  forTargetList:        UndoStacks[F, PointingsWithObs] = UndoStacks.empty[F, PointingsWithObs],
-  forTarget:            Map[Target.Id, UndoStacks[F, TargetResult]] =
+  forObsList:         UndoStacks[F, ObservationList] = UndoStacks.empty[F, ObservationList],
+  forTargetList:      UndoStacks[F, PointingsWithObs] = UndoStacks.empty[F, PointingsWithObs],
+  forTarget:          Map[Target.Id, UndoStacks[F, TargetResult]] =
     Map.empty[Target.Id, UndoStacks[F, TargetResult]],
-  forConstraintList:    UndoStacks[F, ConstraintGroupList] = UndoStacks.empty[F, ConstraintGroupList],
-  forConstraintSet:     Map[Observation.Id, UndoStacks[F, ConstraintSet]] =
-    Map.empty[Observation.Id, UndoStacks[F, ConstraintSet]],
-  forBulkConstraintSet: Map[SortedSet[Observation.Id], UndoStacks[F, ConstraintSet]] =
+  forConstraintList:  UndoStacks[F, ConstraintGroupList] = UndoStacks.empty[F, ConstraintGroupList],
+  forConstraintGroup: Map[SortedSet[Observation.Id], UndoStacks[F, ConstraintSet]] =
     Map.empty[SortedSet[Observation.Id], UndoStacks[F, ConstraintSet]],
-  forScienceData:       Map[Observation.Id, UndoStacks[F, ScienceData]] =
+  forScienceData:     Map[Observation.Id, UndoStacks[F, ScienceData]] =
     Map.empty[Observation.Id, UndoStacks[F, ScienceData]]
 )
 
 object ModelUndoStacks {
-  def forObsList[F[_]]           = Focus[ModelUndoStacks[F]](_.forObsList)
-  def forTargetList[F[_]]        = Focus[ModelUndoStacks[F]](_.forTargetList)
-  def forTarget[F[_]]            = Focus[ModelUndoStacks[F]](_.forTarget)
-  def forConstraintList[F[_]]    = Focus[ModelUndoStacks[F]](_.forConstraintList)
-  def forConstraintSet[F[_]]     = Focus[ModelUndoStacks[F]](_.forConstraintSet)
-  def forBulkConstraintSet[F[_]] = Focus[ModelUndoStacks[F]](_.forBulkConstraintSet)
-  def forScienceData[F[_]]       = Focus[ModelUndoStacks[F]](_.forScienceData)
+  def forObsList[F[_]]         = Focus[ModelUndoStacks[F]](_.forObsList)
+  def forTargetList[F[_]]      = Focus[ModelUndoStacks[F]](_.forTargetList)
+  def forTarget[F[_]]          = Focus[ModelUndoStacks[F]](_.forTarget)
+  def forConstraintList[F[_]]  = Focus[ModelUndoStacks[F]](_.forConstraintList)
+  def forConstraintGroup[F[_]] = Focus[ModelUndoStacks[F]](_.forConstraintGroup)
+  def forScienceData[F[_]]     = Focus[ModelUndoStacks[F]](_.forScienceData)
 
   implicit def eqModelUndoStacks[F[_]]: Eq[ModelUndoStacks[F]] =
     Eq.by(u =>
@@ -45,8 +42,7 @@ object ModelUndoStacks {
        u.forTargetList,
        u.forTarget,
        u.forConstraintList,
-       u.forConstraintSet,
-       u.forBulkConstraintSet,
+       u.forConstraintGroup,
        u.forScienceData
       )
     )

gemini-hlsw/lucuma-apps

explore/common/src/main/scala/explore/model/ModelUndoStacks.scala
@@ -16,28 +16,25 @@ import monocle.Focus
 import scala.collection.immutable.SortedSet
 
 case class ModelUndoStacks[F[_]](
-  forObsList:           UndoStacks[F, ObservationList] = UndoStacks.empty[F, ObservationList],
-  forTargetList:        UndoStacks[F, Nothing] = UndoStacks.empty[F, Nothing],
+  forObsList:         UndoStacks[F, ObservationList] = UndoStacks.empty[F, ObservationList],
+  forTargetList:      UndoStacks[F, Nothing] = UndoStacks.empty[F, Nothing],
   // forTargetList:        UndoStacks[F, PointingsWithObs] = UndoStacks.empty[F, PointingsWithObs],
-  forSiderealTarget:    Map[Target.Id, UndoStacks[F, SiderealTarget]] =
+  forSiderealTarget:  Map[Target.Id, UndoStacks[F, SiderealTarget]] =
     Map.empty[Target.Id, UndoStacks[F, SiderealTarget]],
-  forConstraintList:    UndoStacks[F, ConstraintGroupList] = UndoStacks.empty[F, ConstraintGroupList],
-  forConstraintSet:     Map[Observation.Id, UndoStacks[F, ConstraintSet]] =
-    Map.empty[Observation.Id, UndoStacks[F, ConstraintSet]],
-  forBulkConstraintSet: Map[SortedSet[Observation.Id], UndoStacks[F, ConstraintSet]] =
+  forConstraintList:  UndoStacks[F, ConstraintGroupList] = UndoStacks.empty[F, ConstraintGroupList],
+  forConstraintGroup: Map[SortedSet[Observation.Id], UndoStacks[F, ConstraintSet]] =
     Map.empty[SortedSet[Observation.Id], UndoStacks[F, ConstraintSet]],
-  forScienceData:       Map[Observation.Id, UndoStacks[F, ScienceData]] =
+  forScienceData:     Map[Observation.Id, UndoStacks[F, ScienceData]] =
     Map.empty[Observation.Id, UndoStacks[F, ScienceData]]
 )
 
 object ModelUndoStacks {
-  def forObsList[F[_]]           = Focus[ModelUndoStacks[F]](_.forObsList)
-  def forTargetList[F[_]]        = Focus[ModelUndoStacks[F]](_.forTargetList)
-  def forSiderealTarget[F[_]]    = Focus[ModelUndoStacks[F]](_.forSiderealTarget)
-  def forConstraintList[F[_]]    = Focus[ModelUndoStacks[F]](_.forConstraintList)
-  def forConstraintSet[F[_]]     = Focus[ModelUndoStacks[F]](_.forConstraintSet)
-  def forBulkConstraintSet[F[_]] = Focus[ModelUndoStacks[F]](_.forBulkConstraintSet)
-  def forScienceData[F[_]]       = Focus[ModelUndoStacks[F]](_.forScienceData)
+  def forObsList[F[_]]         = Focus[ModelUndoStacks[F]](_.forObsList)
+  def forTargetList[F[_]]      = Focus[ModelUndoStacks[F]](_.forTargetList)
+  def forSiderealTarget[F[_]]  = Focus[ModelUndoStacks[F]](_.forSiderealTarget)
+  def forConstraintList[F[_]]  = Focus[ModelUndoStacks[F]](_.forConstraintList)
+  def forConstraintGroup[F[_]] = Focus[ModelUndoStacks[F]](_.forConstraintGroup)
+  def forScienceData[F[_]]     = Focus[ModelUndoStacks[F]](_.forScienceData)
 
   implicit def eqModelUndoStacks[F[_]]: Eq[ModelUndoStacks[F]] =
     Eq.by(u =>
@@ -45,8 +42,7 @@ object ModelUndoStacks {
        u.forTargetList,
        u.forSiderealTarget,
        u.forConstraintList,
-       u.forConstraintSet,
-       u.forBulkConstraintSet,
+       u.forConstraintGroup,
        u.forScienceData
       )
     )