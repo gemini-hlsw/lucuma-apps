gemini-hlsw/explore

reinstated Asterism on Aladin components

explore/src/main/scala/explore/targeteditor/AladinCell.scala
@@ -78,13 +78,12 @@ import scala.concurrent.duration.*
 import lucuma.schemas.model.TargetWithId
 
 case class AladinCell(
-  uid:             User.Id,
-  tid:             Target.Id,
-  vizTime:         Instant,
-  obsConf:         Option[ObsConfiguration],
-  asterism:        NonEmptyList[TargetWithId],
-  focusedTargetId: Target.Id,
-  fullScreen:      View[AladinFullScreen]
+  uid:        User.Id,
+  tid:        Target.Id,
+  vizTime:    Instant,
+  obsConf:    Option[ObsConfiguration],
+  asterism:   Asterism,
+  fullScreen: View[AladinFullScreen]
 ) extends ReactFnProps(AladinCell.component):
   val anglesToTest: Option[NonEmptyList[Angle]] =
     for {
@@ -317,24 +316,23 @@ object AladinCell extends ModelOptics with AladinCommon:
                   val params = AgsParams.GmosAgsParams(fpu, PortDisposition.Side)
 
                   val sciencePositions =
-                    props.asterism.toList
-                      .map(_.toSidereal.map(_.target).flatMap(_.tracking.at(vizTime)))
-                      .flattenOption
+                    props.asterism.asList
+                      .flatMap(_.toSidereal)
+                      .flatMap(_.target.tracking.at(vizTime))
 
                   val process = for
                     _ <- agsState.set(AgsState.Calculating).to[IO]
                     _ <-
                       AgsClient[IO]
                         .requestSingle(
-                          AgsMessage.Request(
-                            props.tid,
-                            constraints,
-                            wavelength,
-                            base.value,
-                            sciencePositions,
-                            positions,
-                            params,
-                            candidates
+                          AgsMessage.Request(props.tid,
+                                             constraints,
+                                             wavelength,
+                                             base.value,
+                                             sciencePositions,
+                                             positions,
+                                             params,
+                                             candidates
                           )
                         )
                         .map(_.map(_.sortPositions(positions)))
@@ -536,7 +534,6 @@ object AladinCell extends ModelOptics with AladinCommon:
             case (u: UserGlobalPreferences, t: TargetVisualOptions) =>
               AladinContainer(
                 props.asterism,
-                props.focusedTargetId,
                 props.vizTime,
                 props.obsConf,
                 u.copy(fullScreen = props.fullScreen.get),
@@ -555,13 +552,12 @@ object AladinCell extends ModelOptics with AladinCommon:
               val agsState = props.obsConf
                 .flatMap(_.agsState.map(_.get))
                 .getOrElse(AgsState.Idle)
-              AladinToolbar(
-                Fov(t.fovRA, t.fovDec),
-                mouseCoords.value,
-                agsState,
-                selectedGuideStar,
-                u.aladinAgsOverlay,
-                offsetOnCenter
+              AladinToolbar(Fov(t.fovRA, t.fovDec),
+                            mouseCoords.value,
+                            agsState,
+                            selectedGuideStar,
+                            u.aladinAgsOverlay,
+                            offsetOnCenter
               )
 
           val renderAgsOverlay: ((UserGlobalPreferences, TargetVisualOptions)) => VdomNode =

gemini-hlsw/lucuma-apps

explore/explore/src/main/scala/explore/targeteditor/AladinCell.scala
@@ -78,13 +78,12 @@ import scala.concurrent.duration.*
 import lucuma.schemas.model.TargetWithId
 
 case class AladinCell(
-  uid:             User.Id,
-  tid:             Target.Id,
-  vizTime:         Instant,
-  obsConf:         Option[ObsConfiguration],
-  asterism:        NonEmptyList[TargetWithId],
-  focusedTargetId: Target.Id,
-  fullScreen:      View[AladinFullScreen]
+  uid:        User.Id,
+  tid:        Target.Id,
+  vizTime:    Instant,
+  obsConf:    Option[ObsConfiguration],
+  asterism:   AsterismZipper,
+  fullScreen: View[AladinFullScreen]
 ) extends ReactFnProps(AladinCell.component):
   val anglesToTest: Option[NonEmptyList[Angle]] =
     for {
@@ -107,7 +106,7 @@ case class AladinCell(
   def canRunAGS: Boolean = obsConf.exists(o => o.constraints.isDefined && o.wavelength.isDefined)
 
 trait AladinCommon:
-  given Reusability[Asterism] = Reusability.by(x => (x.toSiderealTracking, x.focus.id))
+  given Reusability[AsterismZipper] = Reusability.by(x => (x.toSiderealTracking, x.focus.id))
   given Reusability[AgsState] = Reusability.byEq
 
 object AladinCell extends ModelOptics with AladinCommon:
@@ -317,24 +316,23 @@ object AladinCell extends ModelOptics with AladinCommon:
                   val params = AgsParams.GmosAgsParams(fpu, PortDisposition.Side)
 
                   val sciencePositions =
-                    props.asterism.toList
-                      .map(_.toSidereal.map(_.target).flatMap(_.tracking.at(vizTime)))
-                      .flattenOption
+                    props.asterism.asList
+                      .flatMap(_.toSidereal)
+                      .flatMap(_.target.tracking.at(vizTime))
 
                   val process = for
                     _ <- agsState.set(AgsState.Calculating).to[IO]
                     _ <-
                       AgsClient[IO]
                         .requestSingle(
-                          AgsMessage.Request(
-                            props.tid,
-                            constraints,
-                            wavelength,
-                            base.value,
-                            sciencePositions,
-                            positions,
-                            params,
-                            candidates
+                          AgsMessage.Request(props.tid,
+                                             constraints,
+                                             wavelength,
+                                             base.value,
+                                             sciencePositions,
+                                             positions,
+                                             params,
+                                             candidates
                           )
                         )
                         .map(_.map(_.sortPositions(positions)))
@@ -536,7 +534,6 @@ object AladinCell extends ModelOptics with AladinCommon:
             case (u: UserGlobalPreferences, t: TargetVisualOptions) =>
               AladinContainer(
                 props.asterism,
-                props.focusedTargetId,
                 props.vizTime,
                 props.obsConf,
                 u.copy(fullScreen = props.fullScreen.get),
@@ -555,13 +552,12 @@ object AladinCell extends ModelOptics with AladinCommon:
               val agsState = props.obsConf
                 .flatMap(_.agsState.map(_.get))
                 .getOrElse(AgsState.Idle)
-              AladinToolbar(
-                Fov(t.fovRA, t.fovDec),
-                mouseCoords.value,
-                agsState,
-                selectedGuideStar,
-                u.aladinAgsOverlay,
-                offsetOnCenter
+              AladinToolbar(Fov(t.fovRA, t.fovDec),
+                            mouseCoords.value,
+                            agsState,
+                            selectedGuideStar,
+                            u.aladinAgsOverlay,
+                            offsetOnCenter
               )
 
           val renderAgsOverlay: ((UserGlobalPreferences, TargetVisualOptions)) => VdomNode =