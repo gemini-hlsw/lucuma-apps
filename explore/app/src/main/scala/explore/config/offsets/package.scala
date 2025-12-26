9b324a718f9631956d3b524a1567cdee8d36190a
Raúl Piaggio <rpiaggio@gemini.edu>
2025-12-26 19:06:56 -0300

    migrating offset editor

--- explore/app/src/main/scala/explore/config/offsets/package.scala
+++ explore/app/src/main/scala/explore/config/offsets/package.scala
@@ -1,52 +1,37 @@
 // Copyright (c) 2016-2025 Association of Universities for Research in Astronomy, Inc. (AURA)
 // For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
 
-// package explore.config.offsets
+package explore.config.offsets
 
-// import cats.Eq
-// import cats.data.NonEmptyList
-// import cats.derived.*
-// import eu.timepit.refined.cats.*
-// import eu.timepit.refined.types.numeric.PosInt
-// import lucuma.core.math.*
-// import lucuma.core.util.Enumerated
-// import monocle.Focus
-// import monocle.Lens
+import cats.Eq
+import cats.data.NonEmptyList
+import cats.derived.*
+import cats.syntax.option.*
+import eu.timepit.refined.cats.*
+import eu.timepit.refined.types.numeric.PosInt
+import lucuma.core.geom.OffsetGenerator
+import lucuma.core.math.*
+import lucuma.core.util.Display
+import lucuma.core.util.Enumerated
+import monocle.Focus
+import monocle.Lens
 
-// sealed trait GridParams derives Eq:
-//   def gridType: GridType
+enum GridType(val tag: String, val shortName: String, val init: Option[OffsetGenerator])
+    derives Enumerated:
+  case NoGrid extends GridType("none", "None", none)
+  case Uniform
+      extends GridType("uniform", "Uniform", OffsetGenerator.Uniform(Offset.Zero, Offset.Zero).some)
+  case Spiral
+      extends GridType("spiral", "Spiral", OffsetGenerator.Spiral(Angle.Angle0, Offset.Zero).some)
+  case Random
+      extends GridType("random", "Random", OffsetGenerator.Random(Angle.Angle0, Offset.Zero).some)
 
-// case class RectangularParams(
-//   rows:  PosInt,
-//   cols:  PosInt,
-//   stepP: Angle,
-//   stepQ: Angle
-// ) extends GridParams derives Eq:
-//   def gridType: GridType = GridType.Rectangular
+object GridType:
+  def fromOffsetGenerator(og: Option[OffsetGenerator]): GridType =
+    og match
+      case None                                => GridType.NoGrid
+      case Some(OffsetGenerator.Uniform(_, _)) => GridType.Uniform
+      case Some(OffsetGenerator.Spiral(_, _))  => GridType.Spiral
+      case Some(OffsetGenerator.Random(_, _))  => GridType.Random
 
-// object RectangularParams:
-//   val rows: Lens[RectangularParams, PosInt] = Focus[RectangularParams](_.rows)
-//   val cols: Lens[RectangularParams, PosInt] = Focus[RectangularParams](_.cols)
-//   val stepP: Lens[RectangularParams, Angle] = Focus[RectangularParams](_.stepP)
-//   val stepQ: Lens[RectangularParams, Angle] = Focus[RectangularParams](_.stepQ)
-
-// case class SpiralParams(
-//   size: Angle
-// ) extends GridParams derives Eq:
-//   def gridType: GridType = GridType.Spiral
-
-// object SpiralParams:
-//   val size: Lens[SpiralParams, Angle] = Focus[SpiralParams](_.size)
-
-// case class RandomParams(
-//   size: Angle // area radius
-// ) extends GridParams derives Eq:
-//   def gridType: GridType = GridType.Random
-
-// object RandomParams:
-//   val size: Lens[RandomParams, Angle] = Focus[RandomParams](_.size)
-
-// enum GridType(val tag: String) derives Enumerated:
-//   case Rectangular extends GridType("rectangular")
-//   case Spiral      extends GridType("spiral")
-//   case Random      extends GridType("random")
+  given Display[GridType] = Display.byShortName(_.shortName)
