135aa2b21810e03f2ca23a5910b89bc03ed4633b
Raúl Piaggio <rpiaggio@gemini.edu>
2025-12-29 10:33:30 -0300

    add random parameters editor

--- explore/app/src/main/scala/explore/config/offsets/OffsetInput.scala
+++ explore/app/src/main/scala/explore/config/offsets/OffsetInput.scala
@@ -4,15 +4,41 @@
 package explore.config.offsets
 
 import crystal.react.View
+import eu.timepit.refined.types.string.NonEmptyString
+import explore.model.ExploreModelValidators
 import japgolly.scalajs.react.*
 import japgolly.scalajs.react.vdom.html_<^.*
 import lucuma.core.math.Offset
 import lucuma.react.common.*
+import lucuma.refined.*
+import lucuma.ui.input.ChangeAuditor
+import lucuma.ui.primereact.FormInputTextView
+import lucuma.ui.primereact.given
+import lucuma.ui.syntax.all.given
 
-final case class OffsetInput(offset: View[Offset], readonly: Boolean)
-    extends ReactFnProps(OffsetInput)
+final case class OffsetInput(id: NonEmptyString, offset: View[Offset], readonly: Boolean)
+    extends ReactFnProps(OffsetInput):
+  val pId: NonEmptyString = NonEmptyString.unsafeFrom(s"${id.value}-p")
+  val qId: NonEmptyString = NonEmptyString.unsafeFrom(s"${id.value}-q")
 
 object OffsetInput
     extends ReactFnComponent[OffsetInput]({ props =>
-      <.div(props.offset.get.toString)
+      React.Fragment(
+        <.label(^.htmlFor := props.pId.value, "p:"),
+        FormInputTextView(
+          id = props.pId,
+          value = props.offset.zoom(Offset.pAngle),
+          validFormat = ExploreModelValidators.decimalArcsecondsValidWedge,
+          changeAuditor = ChangeAuditor.bigDecimal(3.refined, 2.refined),
+          placeholder = "0.0"
+        ),
+        <.label(^.htmlFor := props.qId.value, "q:"),
+        FormInputTextView(
+          id = props.qId,
+          value = props.offset.zoom(Offset.qAngle),
+          validFormat = ExploreModelValidators.decimalArcsecondsValidWedge,
+          changeAuditor = ChangeAuditor.bigDecimal(3.refined, 2.refined),
+          placeholder = "0.0"
+        )
+      )
     })
