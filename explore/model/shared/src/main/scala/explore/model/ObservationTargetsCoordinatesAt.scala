7dca99fdab82349ed55da5e7836bface09f8d237
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2026-01-13 10:40:34 -0300

    Fall back to blind offset base if target is not science

--- explore/model/shared/src/main/scala/explore/model/ObservationTargetsCoordinatesAt.scala
+++ explore/model/shared/src/main/scala/explore/model/ObservationTargetsCoordinatesAt.scala
@@ -56,5 +56,8 @@ object ObservationTargetsCoordinatesAt:
       )
     (eEpoch, eScienceMap, eBlindTuple).mapN: (epoch, scienceMap, blindTuple) =>
       val allMap = blindTuple.fold(scienceMap)(scienceMap + _)
-      val base   = NonEmptyList.fromList(scienceMap.values.toList).map(Coordinates.centerOf)
+      val base   = NonEmptyList
+        .fromList(scienceMap.values.toList)
+        .map(Coordinates.centerOf)
+        .orElse(blindTuple.map(_._2))
       ObservationTargetsCoordinatesAt(epoch, base, blindTuple.map(_._2), allMap, scienceMap)
