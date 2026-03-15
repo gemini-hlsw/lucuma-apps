d062899500f0534f5104b7ee160b721f081d9db4
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2026-03-14 21:04:18 -0300

    Add Igrins2 disabled controller

---
+++ observe/server_new/src/main/scala/observe/server/igrins2/Igrins2ControllerDisabled.scala
@@ -0,0 +1,60 @@
+// Copyright (c) 2016-2025 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package observe.server.igrins2
+
+import cats.Applicative
+import cats.implicits._
+import org.typelevel.log4cats.Logger
+import observe.model.Observation
+import observe.model.dhs.ImageFileId
+import observe.server.overrideLogMessage
+import observe.server.keywords.GdsClient
+import observe.server.keywords.KeywordBag
+import lucuma.core.util.TimeSpan
+import fs2.Stream
+
+class Igrins2ControllerDisabled[F[_]: Logger: Applicative] extends Igrins2Controller[F] {
+  private val name = "IGRINS2"
+
+  override def exposureProgress: F[Stream[F, Int]] =
+    Stream.emit[F, Int](0).pure[F]
+
+  def requestedTime: F[Option[Float]] =
+    none[Float].pure[F]
+
+  def sequenceComplete: F[Unit] = Applicative[F].unit
+
+  def currentStatus: F[Igrins2ControllerState] = Igrins2ControllerState.Idle.pure[F].widen
+
+  def dcIsPreparing: F[Boolean] = false.pure[F]
+
+  def dcIsAcquiring: F[Boolean] = false.pure[F]
+
+  def dcIsReadingOut: F[Boolean] = false.pure[F]
+
+  def dcIsWritingMEF: F[Boolean] = false.pure[F]
+
+  override def gdsClient: GdsClient[F] = new GdsClient[F] {
+    override def setKeywords(id: ImageFileId, ks: KeywordBag): F[Unit] =
+      overrideLogMessage(name, "setKeywords")
+
+    override def openObservation(obsId: Observation.Id, id: ImageFileId, ks: KeywordBag): F[Unit] =
+      overrideLogMessage(name, "openObservation")
+
+    override def closeObservation(id: ImageFileId): F[Unit] =
+      overrideLogMessage(name, "closeObservation")
+
+    override def abortObservation(id: ImageFileId): F[Unit] =
+      overrideLogMessage(name, "abortObservation")
+  }
+
+  override def applyConfig(config: Igrins2Config): F[Unit] = overrideLogMessage(name, "applyConfig")
+
+  override def observe(fileId: ImageFileId, expTime: TimeSpan): F[ImageFileId] =
+    overrideLogMessage(name, s"observe $fileId").as(fileId)
+
+  override def endObserve: F[Unit] = overrideLogMessage(name, "endObserve")
+
+  override def abort: F[Unit] = overrideLogMessage(name, "abort")
+}
