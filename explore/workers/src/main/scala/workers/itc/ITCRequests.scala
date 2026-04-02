6bb717ddb8161867d01f03ddcaad1bb4b5353068
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2026-04-02 10:33:30 -0300

    Invalidate itc cache [sc-8265]

--- explore/workers/src/main/scala/workers/itc/ITCRequests.scala
+++ explore/workers/src/main/scala/workers/itc/ITCRequests.scala
@@ -29,7 +29,7 @@ import queries.schemas.itc.syntax.*
 import workers.*
 
 object ITCRequests:
-  val cacheVersion = CacheVersion(24)
+  val cacheVersion = CacheVersion(25)
 
   val itcErrorToQueryProblems: Error => ItcQueryProblem =
     case Error.SourceTooBright(halfWell)  => ItcQueryProblem.SourceTooBright(halfWell)
