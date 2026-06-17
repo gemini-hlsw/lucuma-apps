d22045a5f3748255b615c41d583db03680f83703
Raúl Piaggio <rpiaggio@gemini.edu>
2026-06-17 12:30:23 -0300

    fix version

--- project/Versions.scala
+++ project/Versions.scala
@@ -35,7 +35,7 @@ object Versions {
   val log4CatsLogLevel       = "0.3.1"
   val log4s                  = "1.10.0"
   val logback                = "1.5.34"
-  val lucumaCore             = "0.211-bb00856-20260617T000912Z-SNAPSHOT"
+  val lucumaCore             = "0.211.0"
   val lucumaPrimeStyles      = "0.5.0"
   val lucumaReact            = "0.102.0"
   val lucumaRefined          = "0.1.4"
