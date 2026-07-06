c581d36375d56457f6a53e436c07c14939d8a7b3
Raúl Piaggio <rpiaggio@gemini.edu>
2026-07-06 15:18:38 -0300

    bump itc cache version

--- explore/workers/src/main/scala/workers/itc/ITCRequests.scala
+++ explore/workers/src/main/scala/workers/itc/ITCRequests.scala
@@ -29,7 +29,7 @@ import queries.schemas.itc.syntax.*
 import workers.*
 
 object ITCRequests:
-  val cacheVersion: CacheVersion = CacheVersion(35)
+  val cacheVersion: CacheVersion = CacheVersion(36)
 
   val itcErrorToQueryProblems: Error => ItcQueryProblem =
     case Error.SourceTooBright(halfWell)  => ItcQueryProblem.SourceTooBright(halfWell)
