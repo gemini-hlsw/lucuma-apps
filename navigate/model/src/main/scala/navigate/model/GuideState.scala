532bb97291e42cd18e11618a07fb6afb47458975
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2026-07-08 14:46:38 -0400

    scalafix

--- navigate/model/src/main/scala/navigate/model/GuideState.scala
+++ navigate/model/src/main/scala/navigate/model/GuideState.scala
@@ -6,7 +6,9 @@ package navigate.model
 import cats.Eq
 import cats.derived.*
 import lucuma.core.enums.MountGuideOption
-import lucuma.core.model.{M1GuideConfig, M2GuideConfig, ProbeGuide}
+import lucuma.core.model.M1GuideConfig
+import lucuma.core.model.M2GuideConfig
+import lucuma.core.model.ProbeGuide
 
 case class GuideState(
   mountOffload:  MountGuideOption,
