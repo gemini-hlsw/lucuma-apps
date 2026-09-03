14ec5d39e98fd9947bfa4a507d62d394c86e027f
Javier Lührs <javier.luhrs@noirlab.edu>
2026-09-03 14:32:48 -0400

    Reverted unintended change in Navigate.

--- navigate/server/src/main/scala/navigate/server/tcs/EcsChannels.scala
+++ navigate/server/src/main/scala/navigate/server/tcs/EcsChannels.scala
@@ -24,7 +24,7 @@ object EcsChannels {
     top:     NonEmptyString
   ): Resource[F, EcsChannels[F]] = for {
     tt  <- service.getChannel[String](top, "health.VAL").map(TelltaleChannel(sysName, _))
-    evg <- service.getChannel[Double](top, "e")
+    evg <- service.getChannel[Double](top, "sad:eastVentGatePos.VAL")
     wvg <- service.getChannel[Double](top, "sad:westVentGatePos.VAL")
   } yield EcsChannels(tt, evg, wvg)
 
