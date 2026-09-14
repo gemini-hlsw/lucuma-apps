294fa667735cb8159d6c867c3cf1f0c2332be8b8
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2026-09-14 16:20:08 -0300

    Reduce sequence generation throttling

--- explore/app/src/main/scala/explore/config/sequence/SequenceTileHelper.scala
+++ explore/app/src/main/scala/explore/config/sequence/SequenceTileHelper.scala
@@ -37,7 +37,7 @@ object SequenceTileHelper:
         useEffectKeepResultOnMount:
           ctx.odbApi.sequenceData(obsId, calibrationRole.forall(_.needsITC))
       refreshVisits                           <- useThrottledCallback(5.seconds)(visits.refresh.value.to[IO])
-      refreshSequence                         <- useThrottledCallback(7.seconds)(sequenceData.refresh.to[IO])
+      refreshSequence                         <- useThrottledCallback(2.seconds)(sequenceData.refresh.to[IO])
       _                                       <-
         useEffectStreamResourceOnMount: // Subscribe to observation edits
           ctx.odbApi
